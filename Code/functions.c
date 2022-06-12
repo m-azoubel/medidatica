51bf7788164cb1cd10bc9cd400985e362d1ad950
Artur Teodoro <artur.luiz.teodoro@hotmail.com>
2022-06-12 16:15:23 -0300

    add addQuestion function | created rascunho.c

--- Code/functions.c
+++ Code/functions.c
@@ -62,67 +62,41 @@ int verifyUser(char * cpf, User* users){
 //--------------------------------------------------------------------------//
 
 
-typedef struct Questao
+typedef struct Question
 {
     char enunciado[150];
     char alternativa_correta[150];
     char alternativa2[150];
     char alternativa3[150];
-}Questao;
+}Question;
 
 
 
+Question addQuestion(Question questions, char *enunciado)
+{
+    strcpy(questions.enunciado,enunciado);
+    strcpy(questions.alternativa_correta,"pipipi popopo");
+    strcpy(questions.alternativa2,"pipipi popopo2");
+    strcpy(questions.alternativa3,"pipipi popopo3");
+    
+    return questions;
+}
 
 int quiz()
 {
-    Questao perguntas[10];
+    Question perguntas[10];
 
     //char alternativa_usuario[150];
 
     int alternativa_usuario;
 
-    int aleatorio = 1;
-
-    int aux = 2;
-    int aux3 = 1;
-    
-
-    int list_aux[3] = {1,2,3};
-
-    srand(time(NULL));
-
-    
-    for (int i=0; i<150 ; i++)
-    {
-        perguntas[0].enunciado[i] = ' ';
-    }
-    printf("escreva a pergunta 1\n");
-    scanf("%[^\n]",perguntas[0].enunciado);
-    getchar();
-
-    for (int i=0; i<150 ; i++)
-    {
-        printf("%c",perguntas[0].enunciado[i]);
-    }
-
-    printf("\nescreva a alternativa 1\n");
-    scanf("%[^\n]",perguntas[0].alternativa_correta);
-    getchar();
-
-    printf("escreva a alternativa 2\n");
-    scanf("%[^\n]",perguntas[0].alternativa2);
-    getchar();
-
-    printf("escreva a alternativa 3\n");
-    scanf("%[^\n]",perguntas[0].alternativa3);
-    getchar();
-    
     printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
     printf("alternativas: \n");
     printf("1- %s\n",perguntas[0].alternativa_correta);
     printf("2- %s\n",perguntas[0].alternativa2);
     printf("3- %s\n",perguntas[0].alternativa3);
     printf("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+
     scanf("%i",&alternativa_usuario);
     getchar();
     switch (alternativa_usuario){
